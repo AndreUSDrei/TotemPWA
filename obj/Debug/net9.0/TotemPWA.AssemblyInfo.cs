2188c230bdbfda05fd17bea2ebd859eb5867b88d
AndreUSDrei <andreus.viniguina@gmail.com>
2025-03-10 13:16:30 -0300

    Edit image from Index Burguer

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1f6d0f2e13816ecbb23104a5ecd0e6698c27b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe54636f2caf0ebe340f8d6f31d996d7f07bd4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
