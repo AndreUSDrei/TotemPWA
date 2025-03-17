6f5f3bebc2dbfb17a08d5a93ec13e8548ad6d16c
AndreUSDrei <andreus.viniguina@gmail.com>
2025-03-17 13:32:35 -0300

    ajust response

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea251f54aba2b8db5b8bacac130da5055a82a14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4d137dc0c2828492af3ab24570a696112c6fe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
