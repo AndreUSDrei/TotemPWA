73f73135af2d882740bcc168261294744d75548c
Azyrhus <cauazinhoanogueira@gmail.com>
2025-03-31 00:02:31 -0300

    ModalEdit

--- wwwroot/js/modal.js
+++ wwwroot/js/modal.js
@@ -1,9 +1,16 @@
-const openModalBtn = document.getElementById('botaoCarrinho');
-const closeModalBtn = document.getElementById('fecharModal');
-const modal = document.getElementById('modalCarrinho');    
-openModalBtn.addEventListener('click', () => {
-    modal.classList.add('active'); 
-});
-closeModalBtn.addEventListener('click', () => {
-    modal.classList.remove('active');
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("modalCarrinho");
+    const botaoCarrinho = document.getElementById("botaoCarrinho");
+    const fecharModal = document.getElementById("fecharModal");
+    botaoCarrinho.addEventListener("click", function () {
+        modal.style.display = "flex";
+    });
+    fecharModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
 });
