136406889d94794261bc498f950ecb2ac0467a5b
Azyrhus <cauazinhoanogueira@gmail.com>
2025-03-31 10:00:22 -0300

    ModalJsCshtml

--- wwwroot/js/modal.js
+++ wwwroot/js/modal.js
@@ -13,4 +13,32 @@ document.addEventListener("DOMContentLoaded", function () {
             modal.style.display = "none";
         }
     });
+    const botaoAdicionar = document.querySelectorAll('.botaoAdiciona');
+    const botaoRemover = document.querySelectorAll('.botaoRemove');
+    function atualizarIngredienteQuantidade(elemento, operacao) {
+        const IngredienteQuantidade = elemento.parentElement.querySelector('.IngredienteQuantidade');
+        let valorAtual = parseInt(IngredienteQuantidade.innerText, 10); 
+        if (isNaN(valorAtual)) {
+            valorAtual = 0;
+        }
+        if (operacao === 'add') {
+            valorAtual++;
+        } 
+        else if (operacao === 'remove') {
+            if (valorAtual > 0) {
+                valorAtual--;
+            }
+        }
+        IngredienteQuantidade.innerText = `${valorAtual}x`;
+    }
+    botaoAdicionar.forEach((botao) => {
+        botao.addEventListener('click', function () {
+            atualizarIngredienteQuantidade(botao, 'add');
+        });
+    });
+    botaoRemover.forEach((botao) => {
+        botao.addEventListener('click', function () {
+            atualizarIngredienteQuantidade(botao, 'remove');
+        });
+    });
 });
