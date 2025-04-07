455feb533eb02273ef4a35b6b8882010b9d868d9
Thomas-1610 <thomaslcl.1610@gmail.com>
2025-04-07 17:38:34 -0300

    test

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,19 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eecc25ac017db36a38b1da25be41220f7656981")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c84e6d932007bd794a0405886cfee3c0c6b0f27")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08f28fd161b7399996e78dc50ba7dd0b36fee16")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00922311dc34677a7874130306d80c3780a1d78c")]
->>>>>>> 6c84e6d932007bd794a0405886cfee3c0c6b0f27
->>>>>>> fbf9c7c2b2f1bda6a614f791c8912506d2cc2da5
->>>>>>> 5fdca02075d9dc37734d04050f1aee28269a5623
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5eb3249f33d7929c1d3d46eb9b5207cec6f610")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
