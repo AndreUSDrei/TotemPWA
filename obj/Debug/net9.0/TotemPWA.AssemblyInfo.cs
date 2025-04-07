3282f2a93391b64abf7c70d5addcf7b7fb58885d
AndreUSDrei <andreus.viniguina@gmail.com>
2025-04-07 17:06:45 -0300

    review news changes

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00922311dc34677a7874130306d80c3780a1d78c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c84e6d932007bd794a0405886cfee3c0c6b0f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
