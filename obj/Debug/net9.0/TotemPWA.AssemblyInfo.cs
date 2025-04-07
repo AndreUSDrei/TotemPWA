e58acd4f5028d53a479efca686bda69a5aee03ea
Thomas-1610 <thomaslcl.1610@gmail.com>
2025-04-07 15:21:36 -0300

    teste index

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e5a9ede0e64561d636d92cf266346ee3308dc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08f28fd161b7399996e78dc50ba7dd0b36fee16")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
